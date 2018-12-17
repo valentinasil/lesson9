8046b1af5719e2e91d3199b9f23f0dfe398b3505
valentinasil <43757543+valentinasil@users.noreply.github.com>
2018-12-17 12:48:03 -0500

    Add files via upload

---
+++ lesson9/problem2.py
@@ -0,0 +1,11 @@
+print('-'*60)
+print('locker bot:')
+print()
+password = input('what is the password? ')
+if password =='open sesame':
+	print('password is valid!')
+	print('locker opening!')
+else:
+	print('password invalid...enabling laser defense system, please stand down.')
+	print('pew pew pew!')
+print('-'*60)
---
+++ lesson9/problem3.py
@@ -0,0 +1,11 @@
+print('-'*60)
+print('Alien Greeter Bot:')
+print()
+password = input('what planet are you from? ')
+if password =='mars':
+	print('welcome to Earth, please do not kill us!')
+	
+else:
+	print('Dammmmnnnnn, your face is so ugly I tought you were an allien. Anyway...have a good day!')
+	
+print('-'*60)
